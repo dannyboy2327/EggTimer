3fe10dd2b44fbee118b0894a904bee55a50b5c49
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-02-10 19:18:58 -0800

    feat:added a notification builder to the extended function to create the notification

--- app/src/main/java/com/example/eggtimer/util/NotificationsUtil.kt
+++ app/src/main/java/com/example/eggtimer/util/NotificationsUtil.kt
@@ -2,6 +2,14 @@ package com.example.eggtimer.util
 
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.example.eggtimer.R
+
+// Notification ID.
+private val NOTIFICATION_ID = 0
+private val REQUEST_CODE = 0
+private val FLAGS = 0
 
 /**
  * Builds and delivers the notification.
@@ -10,4 +18,15 @@ import android.content.Context
  */
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
 
+    val intent = Intent()
+
+    val builder = NotificationCompat.Builder(
+        applicationContext,
+        applicationContext.getString(R.string.egg_notification_channel_id)
+    )
+        .setSmallIcon(R.drawable.cooked_egg)
+        .setContentTitle(applicationContext.getString(R.string.notification_title))
+        .setContentText(messageBody)
+
+    notify(NOTIFICATION_ID, builder.build())
 }
