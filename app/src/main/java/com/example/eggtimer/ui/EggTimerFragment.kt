31d822a1ae516f5c64ca4a8c60f8dc4bde81a7dd
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-02-11 18:33:35 -0800

    feat: added a new channel for the firebase notifications

--- app/src/main/java/com/example/eggtimer/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/eggtimer/ui/EggTimerFragment.kt
@@ -40,6 +40,11 @@ class EggTimerFragment : Fragment() {
         createChannel(getString(R.string.egg_notification_channel_id),
                         getString(R.string.breakfast_notification_channel_name))
 
+        createChannel(
+            getString(R.string.breakfast_notification_channel_id),
+            getString(R.string.breakfast_notification_channel_name)
+        )
+
         return binding.root
     }
 
